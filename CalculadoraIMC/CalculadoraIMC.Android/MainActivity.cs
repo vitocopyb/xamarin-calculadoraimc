5c0e1f9f1981c728fbceff60e76f3ca6d34b2b0d
Victor Sepulveda <vs@gausscontrol.com>
2022-03-24 18:54:02 -0300

    Instalacion de paquetes Acr.UserDialogs (para activity indicator), DebugRainbows (para asignar un color de fondo a todos los elementos de una pagina)

--- CalculadoraIMC/CalculadoraIMC.Android/MainActivity.cs
+++ CalculadoraIMC/CalculadoraIMC.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Acr.UserDialogs;
 
 namespace CalculadoraIMC.Droid
 {
@@ -15,8 +16,9 @@ namespace CalculadoraIMC.Droid
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+            UserDialogs.Init(this);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- CalculadoraIMC/CalculadoraIMC/MainPage.xaml.cs
+++ CalculadoraIMC/CalculadoraIMC/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Acr.UserDialogs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -17,6 +18,8 @@ namespace CalculadoraIMC
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
+            //UserDialogs.Instance.ShowLoading("Calculando");
+
             string sAltura = Altura.Text;
             string sPeso = Peso.Text;
 
@@ -26,15 +29,37 @@ namespace CalculadoraIMC
                 double peso = double.Parse(sPeso);
                 double imc = peso / Math.Pow(altura, 2);
 
+                // coloca un delay para esperar unos segundos
+                bool cancelada = false;
+                using (IProgressDialog dialog = UserDialogs.Instance.Progress("Calculando", () => cancelada = true, "Cancelar"))
+                {
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        await Task.Delay(300);
+
+                        if (!cancelada)
+                        {
+                            dialog.PercentComplete = i * 10;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                //await Task.Delay(3000);
                 IMC.Text = Math.Round(imc, 2).ToString();
 
                 // obtiene resultado del IMC
                 string resultado = ResultadoIMC(imc);
 
+                //UserDialogs.Instance.HideLoading();
                 await DisplayAlert("Resultado", resultado, "Aceptar");
             }
             else
             {
+                //UserDialogs.Instance.HideLoading();
                 await DisplayAlert("Datos erróneos", "Por favor, ingrese toda la información", "Aceptar");
             }
         }
